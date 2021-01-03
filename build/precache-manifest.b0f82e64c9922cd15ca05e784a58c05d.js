dcb9e7f8dee6918027e673555cfdec7fa909e29a
Akhilesh Kumar Mishra <52692377+Akhileshtrd@users.noreply.github.com>
2021-01-03 13:54:08 +0530

    Add files via upload

---
+++ build/precache-manifest.b0f82e64c9922cd15ca05e784a58c05d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeea4a5a2ac569d6af3b9daf4f3ee3b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77d23d7999a0fa9e9227",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "44c4d432dda18c463e96",
+    "url": "/static/css/main.77adaff2.chunk.css"
+  },
+  {
+    "revision": "77d23d7999a0fa9e9227",
+    "url": "/static/js/2.0607c148.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0607c148.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44c4d432dda18c463e96",
+    "url": "/static/js/main.22651c6a.chunk.js"
+  },
+  {
+    "revision": "1dc10d092edade72e380",
+    "url": "/static/js/runtime-main.539fc815.js"
+  },
+  {
+    "revision": "ba81b9290108778fd6e05fd1e56a5dd6",
+    "url": "/static/media/im1.ba81b929.jpg"
+  },
+  {
+    "revision": "5b85fb164bc0fa4906583d23e4add8ec",
+    "url": "/static/media/im2.5b85fb16.jpg"
+  },
+  {
+    "revision": "9079986b1d8406f118d9ff782ae4934d",
+    "url": "/static/media/im3.9079986b.jpg"
+  },
+  {
+    "revision": "fe446d54b86bcc577f09b1c036f31637",
+    "url": "/static/media/im4.fe446d54.jpg"
+  },
+  {
+    "revision": "4e8677b56d297522cc85957378660507",
+    "url": "/static/media/im5.4e8677b5.jpg"
+  },
+  {
+    "revision": "6b4cf5ec2d28d3019865cddbc5ede954",
+    "url": "/static/media/im6.6b4cf5ec.jpg"
+  },
+  {
+    "revision": "8591e4eee78e3d17c5b55f953fe8ed36",
+    "url": "/static/media/im7.8591e4ee.jpg"
+  },
+  {
+    "revision": "74f526c7a9d1dd7d371dc4dc55b0bbf8",
+    "url": "/static/media/im8.74f526c7.jpg"
+  },
+  {
+    "revision": "eef056d64564eac0a3b79512e3072c1e",
+    "url": "/static/media/im9.eef056d6.jpg"
+  }
+]);
